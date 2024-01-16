5f869dbd4c8112b29c5305a21c62d8bc2c870f5d
Ricardo Luiz Ferreira <ferreirarl@mdck.local>
2024-01-16 14:44:33 -0300

    Initial commit

---
+++ src/main/java/com/example/basededadosses/BaseDeDadosSesApplication.java
@@ -0,0 +1,13 @@
+package com.example.basededadosses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDeDadosSesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseDeDadosSesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basededadosses/BaseDeDadosSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basededadosses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDeDadosSesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
